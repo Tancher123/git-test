ad2437e710e425e2e3edc4ed0f43d7194722802b
ctc <1299466140@qq.com>
2022-11-14 17:43:52 +0800

    second commit

--- src/main/java/com/ctc/git/TestGit.java
+++ src/main/java/com/ctc/git/TestGit.java
@@ -10,5 +10,6 @@ package com.ctc.git;
 public class TestGit {
     public static void main (String[] args) {
         System.out.println ( "hello,Git" );
+        System.out.println ( "hello,Git1" );
     }
 }
