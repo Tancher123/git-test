36529b9052cefd34fd4fea4ef1f5b6e7b365bbd2
ctc <1299466140@qq.com>
2022-11-14 17:37:10 +0800

    first commit

---
+++ src/main/java/com/ctc/git/TestGit.java
@@ -0,0 +1,14 @@
+package com.ctc.git;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: 陈天赐
+ * @create: 2022-11-14 17:27
+ * @version:1.0
+ **/
+public class TestGit {
+    public static void main (String[] args) {
+        System.out.println ( "hello,Git" );
+    }
+}
