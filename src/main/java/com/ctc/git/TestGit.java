94024cc812a246864646147a1de3fa301cbd6b05
ctc <1299466140@qq.com>
2022-11-14 17:44:21 +0800

    third commit

--- src/main/java/com/ctc/git/TestGit.java
+++ src/main/java/com/ctc/git/TestGit.java
@@ -11,5 +11,6 @@ public class TestGit {
     public static void main (String[] args) {
         System.out.println ( "hello,Git" );
         System.out.println ( "hello,Git1" );
+        System.out.println ( "hello,Git2" );
     }
 }
